e950523a11fefe20c1fe3dfa4a002c472b2880b6
xinguohua <794807072@qq.com>
2024-04-01 19:13:20 +0800

    更改

--- 5-StackOverFlow.cpp
+++ 5-StackOverFlow.cpp
@@ -1,53 +1,53 @@
-///usr/local/clang-4.0/bin/clang++ -emit-llvm -g -c -std=c++14 5-StackOverFlow.cpp -o 5-StackOverFlow.bc
-// Created by nsas2020 on 24-3-15.
-//http://stackoverflow:com/questions/16159203/
+/////usr/local/clang-4.0/bin/clang++ -emit-llvm -g -c -std=c++14 5-StackOverFlow.cpp -o 5-StackOverFlow.bc
+//// Created by nsas2020 on 24-3-15.
+////http://stackoverflow:com/questions/16159203/
+////
+//#include <iostream>
+//#include <thread>
+//#include <mutex>
+//#include <memory>
 //
-#include <iostream>
-#include <thread>
-#include <mutex>
-#include <memory>
-
-using namespace std;
-
-class Point {
-public:
-    int x, y;
-    Point(int x, int y) : x(x), y(y) {}
-};
-
-class A {
-public:
-    static unique_ptr<Point> currentPos;
-    static mutex mtx;
-
-    static void f() {
-        lock_guard<mutex> guard(mtx);
-        if (currentPos->x + 1 != currentPos->y) {
-            cout << currentPos->x << "------- " << currentPos->y << endl;
-            exit(1);
-        }
-    }
-
-    static void threadFunction() {
-        while (true) {
-            f();
-        }
-    }
-};
-
-unique_ptr<Point> A::currentPos = make_unique<Point>(1, 2);
-mutex A::mtx;
-
-int main() {
-    thread t(A::threadFunction);
-
-    while (true) {
-        lock_guard<mutex> guard(A::mtx);
-        int newX = A::currentPos->x + 1;
-        int newY = A::currentPos->y + 1;
-        A::currentPos = make_unique<Point>(newX, newY);
-    }
-
-    t.join(); // This will never be reached
-    return 0;
-}
+//using namespace std;
+//
+//class Point {
+//public:
+//    int x, y;
+//    Point(int x, int y) : x(x), y(y) {}
+//};
+//
+//class A {
+//public:
+//    static unique_ptr<Point> currentPos;
+//    static mutex mtx;
+//
+//    static void f() {
+//        lock_guard<mutex> guard(mtx);
+//        if (currentPos->x + 1 != currentPos->y) {
+//            cout << currentPos->x << "------- " << currentPos->y << endl;
+//            exit(1);
+//        }
+//    }
+//
+//    static void threadFunction() {
+//        while (true) {
+//            f();
+//        }
+//    }
+//};
+//
+//unique_ptr<Point> A::currentPos = make_unique<Point>(1, 2);
+//mutex A::mtx;
+//
+//int main() {
+//    thread t(A::threadFunction);
+//
+//    while (true) {
+//        lock_guard<mutex> guard(A::mtx);
+//        int newX = A::currentPos->x + 1;
+//        int newY = A::currentPos->y + 1;
+//        A::currentPos = make_unique<Point>(newX, newY);
+//    }
+//
+//    t.join(); // This will never be reached
+//    return 0;
+//}
---
+++ 5-StackOverFlowEasy.c
@@ -0,0 +1,65 @@
+// /usr/local/clang-4.0/bin/clang -emit-llvm -g -c 5-StackOverFlowEasy.c -o 5-StackOverFlow.bc
+//// Created by nsas2020 on 24-3-15. 36:20 23:34
+////http://stackoverflow:com/questions/16159203/
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+// 用结构体代替C++中的Point类
+typedef struct {
+    int x, y;
+} Point;
+
+// 全局变量来模拟C++中的静态成员变量
+Point *currentPos;
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+// 更新位置的函数，类似于C++中的A::f()
+void* f(void *arg) {
+    while (currentPos != NULL) {
+        pthread_mutex_lock(&mtx);
+        if (currentPos->x + 1 != currentPos->y) {
+            printf("%d------- %d\n", currentPos->x, currentPos->y);
+            exit(1);
+        }
+        pthread_mutex_unlock(&mtx);
+    }
+}
+
+// 线程函数，模拟C++中的threadFunction
+void* threadFunction(void *arg) {
+    while (1) {
+        Point* addr = malloc(sizeof(Point));
+        addr->x = currentPos->x + 1;
+        addr->y = currentPos->y + 1;
+        currentPos = addr;
+    }
+}
+
+int main() {
+    pthread_t t1, t2;
+
+    // 初始化currentPos
+    currentPos = malloc(sizeof(Point));
+    currentPos->x = 1;
+    currentPos->y = 2;
+
+    // 创建线程
+    if (pthread_create(&t1, NULL, &f, NULL)) {
+        fprintf(stderr, "Error creating thread\n");
+        return 1;
+    }
+
+    if (pthread_create(&t2, NULL, &threadFunction, NULL)) {
+        fprintf(stderr, "Error creating thread\n");
+        return 1;
+    }
+
+    // 等待线程结束，虽然实际上永远不会到达这里
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    free(currentPos);
+    return 0;
+}
